d844f95072c220998f1fcf2e9032e9fd96692c5c
lihaoran <8726682@qq.com>
2018-10-11 23:18:31 +0800

    渐变导航栏

--- Wechat/app/components/MyNavBar.js
+++ Wechat/app/components/MyNavBar.js
@@ -16,32 +16,39 @@ const AnimatedSafeAreaView = Animated.createAnimatedComponent(SafeAreaView)
 
 const MyStatusBar = ({ backgroundColor, topBarOpacity, ...props }) => {
   return (
-    <AnimatedSafeAreaView
-      style={[
-        styles.headerStyle,
-        { backgroundColor: backgroundColor, opacity: topBarOpacity }
-      ]}
-    >
-      <View>
-        {/* 标题栏 */}
+    <View style={styles.headerStyle}>
+      <AnimatedSafeAreaView
+        style={[
+          styles.headerStyle,
+          { backgroundColor: backgroundColor, opacity: topBarOpacity }
+        ]}
+      >
+        <View style={styles.titleBar} />
+      </AnimatedSafeAreaView>
+      <SafeAreaView
+        style={[styles.headerStyle, { backgroundColor: 'transparent' }]}
+      >
+        <View>
+          {/* 标题栏 */}
 
-        <View style={styles.titleBar}>
-          <Text
-            style={styles.navButtonStyle}
-            onPress={() => {
-              props.dispatch(NavigationActions.back())
-            }}
-          >
-            返回
-          </Text>
+          <View style={styles.titleBar}>
+            <Text
+              style={styles.navButtonStyle}
+              onPress={() => {
+                props.dispatch(NavigationActions.back())
+              }}
+            >
+              返回
+            </Text>
 
-          <Text style={styles.titleText}>朋友圈</Text>
+            <Text style={styles.titleText}>朋友圈</Text>
 
-          <Text style={styles.navButtonStyle}>拍照</Text>
+            <Text style={styles.navButtonStyle}>拍照</Text>
+          </View>
+          {/* 用户信息栏 */}
         </View>
-        {/* 用户信息栏 */}
-      </View>
-    </AnimatedSafeAreaView>
+      </SafeAreaView>
+    </View>
   )
 }
 
